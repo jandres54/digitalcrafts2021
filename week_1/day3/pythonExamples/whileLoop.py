83a0f8d3cb9ffc216057c8ee3f80e7a330dfe5f4
Jorge <jandres.jago54@gmail.co>
2021-03-10 23:50:43 -0600

    intro

--- week_1/day3/pythonExamples/whileLoop.py
+++ week_1/day3/pythonExamples/whileLoop.py
@@ -1,3 +1,23 @@
-a = 3
-b = 4
-c = 7
+firstName = input("What is your first name?")
+lengthOfFirstName = len(firstName)
+while(lengthOfFirstName < 1):
+    firstName = input("What is your first name?")
+    lengthOfFirstName = len(firstName)
+lastName = input("What is your last name?")
+lengthOfLastName = len(lastName)
+while(lengthOfLastName < 1):
+    lastName = input("What is your last name?")
+    lengthOfLastName = len(lastName)
+print("The user name is %s %s" % (firstName, lastName))
+friendsFirstName = input("What is your friends first name?")
+lengthOfFriendsFirstName = len(friendsFirstName)
+while(lengthOfFriendsFirstName < 1):
+    friendsFirstName = input("What is your friends first name?")
+    lengthOfFriendsFirstName = len(friendsFirstName)
+friendsLastName = input("What is your friends last name?")
+lengthOfFriendsLastName = len(friendsLastName)
+while(lengthOfFriendsLastName < 1):
+    friendsLastName = input("What is your friends last name?")
+    lengthOfFriendsLastName = len(friendsLastName)
+print("The friends name is %s %s" % (friendsFirstName, friendsLastName))
+print("")
