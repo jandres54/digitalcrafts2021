49e8c1ee21e056769bb9ccd177825a280c65bbbb
Jorge <78658812+jandres54@users.noreply.github.com>
2021-04-26 22:45:08 -0500

    crud cm

--- week_8/day1/classwork/server/db.js
+++ week_8/day1/classwork/server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Aequelina66",
+  password: "",
   host: "localhost",
   port: 5432,
   database: "groceries",
--- week_8/day1/classwork/server/index.js
+++ week_8/day1/classwork/server/index.js
@@ -6,21 +6,65 @@ const pool = require("./db.js");
 
 
 
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Welcome to node server");
+  });
+
+  //create
 app.post("/item1", async (req, res) => {
     try {
-    const { food_items } = req.body
+    const {description} = req.body
     const add_items = await pool.query(
-        "INSERT INTO groceries (food_items) VALUES($1)",
-        [food_items])
-    
+        "INSERT INTO grocery_list (description) VALUES($1)",
+        [description])
     res.json(add_items)
-
     console.log(req.body)}
-    
     catch(err) {
         console.log(err.message)}
 
+  });
+
+//read
+  app.get("/list_items", async (req, res) => {
+    try {
+    const read_items = await pool.query(
+        "SELECT * from grocery_list ORDER BY food_items",);
+    res.json(read_items.rows);}
+    catch(err) {
+        console.log(err.message)}
+  });
+
+//update
+app.put("/update_list/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { description } = req.body;
+  
+      const updateList = await pool.query(
+        "UPDATE grocery_list SET description = $1 WHERE food_items = $2",
+        [description, id]
+      );
+      res.json("Grocery list has been updated!");
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+//delete
+app.delete("/delete_Item/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteItem = await pool.query(
+        "DELETE FROM grocery_list WHERE food_items = $1",
+        [id]
+      );
+      res.json("Item was successfully deleted!");
+    } catch (err) {
+      console.log(err.message);
+    }
   });
 
 
