2b24676b116aa81234582306b73f29f55a32a045
Jorge <78658812+jandres54@users.noreply.github.com>
2021-05-18 15:40:37 -0500

    passing data

---
+++ week_11/day1/google-home/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+      <div className = "app">
+        <Navbar />
+        <Container />
+        <Footer />
+      </div>
+    );
+  }
+
+export default App;
