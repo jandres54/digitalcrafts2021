b6336e82a59ab8566ad9a04f42eaf86ed0db7118
Jorge <78658812+jandres54@users.noreply.github.com>
2021-05-26 15:26:09 -0500

    count & user

--- week_12/day3/re-dux-app/src/Components/Counter.js
+++ week_12/day3/re-dux-app/src/Components/Counter.js
@@ -2,24 +2,15 @@ import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 
 const Counter = () => {
-    const counter = useSelector((state)=> state.counter);
     const userName = useSelector((state)=> state.userName);
-    const poop = useSelector((state)=> state.poop);
-
+    
     const dispatch = useDispatch();
     return (
         <div>
-            <h1>Counter</h1>
-            <p>{counter}</p>
+            <h1>User</h1>
             <p>{userName}</p>
-            <p>{poop}</p>
-            <button onClick={()=>dispatch({type:"ADD"})}>Add</button>
-            <button onClick={()=>dispatch({type:"SUBTRACT"})}>Subtract</button>
-
-            {/* <input type="text" onChange={(e) =>
-            dispatch({type: "CHANGE_POOP_TO_DUCK", value: e.target.value})}
-            /> */}
-
+            <button type="text" onClick={(e) =>
+            dispatch({type: "CHANGE_JORGE_TO_JOE", value: e.target.value})}>Switch</button>
         </div>
     );
 }
--- week_12/day3/re-dux-app/src/reducers/rootReducer.js
+++ week_12/day3/re-dux-app/src/reducers/rootReducer.js
@@ -1,20 +1,12 @@
 const initialState = {
-    counter: 0,
     userName: "Jorge",
-    address: {},
-    poop: "💩",
+    
 };
 
 function rootReducer(state = initialState, action){
     switch(action.type){
-        case "ADD":
-            return {...state,counter: state.counter + 1};
-            
-        case "SUBTRACT": 
-            return {...state,counter: state.counter - 1};
-
-        case "CHANGE_POOP_TO_DUCK": 
-            return {...state,poop: "🦆"};
+        case "CHANGE_JORGE_TO_JOE": 
+            return {userName: "Joe"};
         default:
             return state
     }
